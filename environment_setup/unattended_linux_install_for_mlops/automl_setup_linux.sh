c1e39cf7b936d8fb739535070983a9a4cf4726d3
jostrm <jostrm@microsoft.com>
2022-05-02 22:54:14 +0200

    conda

--- environment_setup/unattended_linux_install_for_mlops/automl_setup_linux.sh
+++ environment_setup/unattended_linux_install_for_mlops/automl_setup_linux.sh
@@ -15,7 +15,7 @@ conda init bash # TODO Add this
 
 if [ "$CONDA_ENV_NAME" == "" ]
 then
-  CONDA_ENV_NAME="azure_automl_esml"
+  CONDA_ENV_NAME="azure_automl_esml_v13"
 fi
 
 if [ "$AUTOML_ENV_FILE" == "" ]
