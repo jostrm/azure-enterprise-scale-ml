7818907f0dc950720528bcebf5eaaa692f0b3c78
Ben Kooijman <ben.kooijman@external.epiroc.com>
2022-05-30 14:20:08 +0200

    Updated with best practice according to SC2086

--- environment_setup/automl_setup_linux.sh
+++ environment_setup/automl_setup_linux.sh
@@ -10,7 +10,7 @@ PIP_NO_WARN_SCRIPT_LOCATION=0
 CHECK_CONDA_VERSION_SCRIPT="check_conda_version.py"
 
 ls -l
-echo "2_Currentd dir:" $PWD
+echo "2_Currentd dir:" "${PWD}"
 #echo "##vso[task.prependpath]$CONDA/bin"
 #echo "##vso[task.prependpath]$env:CONDA\Scripts" # Bin might be enough
 
@@ -18,18 +18,18 @@ conda init bash
 conda init powershell # modifies the configs for the shells...to find conda in path
 echo "Anna"
 
-if [ "$CONDA_ENV_NAME" == "" ]
+if [ "${CONDA_ENV_NAME}" == "" ]
 then
   CONDA_ENV_NAME="azure_automl"
 fi
 
-if [ "$AUTOML_ENV_FILE" == "" ]
+if [ "${AUTOML_ENV_FILE}" == "" ]
 then
   AUTOML_ENV_FILE="automl_env_linux.yml"
 fi
 
 if [ ! -f $AUTOML_ENV_FILE ]; then
-    echo "File $AUTOML_ENV_FILE not found"
+    echo "File ${AUTOML_ENV_FILE} not found"
     exit 1
 fi
 
@@ -47,20 +47,20 @@ fi
 # automl_setup_linux.sh: line 51: activate: No such file or directory
 
 #TODO: not nessisary...does not exists
-sed -i 's/AZUREML-SDK-VERSION/latest/' $AUTOML_ENV_FILE
+sed -i 's/AZUREML-SDK-VERSION/latest/' "${AUTOML_ENV_FILE}"
 
-if conda activate $CONDA_ENV_NAME 2> /dev/null
+if conda activate "${CONDA_ENV_NAME}" 2> /dev/null
 then
-   echo "Upgrading existing conda environment" $CONDA_ENV_NAME
+   echo "Upgrading existing conda environment" "${CONDA_ENV_NAME}"
    pip uninstall azureml-train-automl -y -q
-   conda env update --name $CONDA_ENV_NAME --file $AUTOML_ENV_FILE &&
+   conda env update --name "${CONDA_ENV_NAME}" --file "${AUTOML_ENV_FILE}" &&
    jupyter nbextension uninstall --user --py azureml.widgets
 else
    echo "Conda version"
    conda --version
    ls -l
-   echo "2_Currentd dir:" $PWD
-   conda env create -f $AUTOML_ENV_FILE -n $CONDA_ENV_NAME &&
+   echo "2_Currentd dir:" "${PWD}"
+   conda env create -f "${AUTOML_ENV_FILE}" -n "${CONDA_ENV_NAME}" &&
    
    conda env list
    # conda activate $CONDA_ENV_NAME  # https://github.com/conda/conda-build/issues/3371
@@ -70,7 +70,7 @@ else
 
    eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
    #conda init bash
-   conda activate $CONDA_ENV_NAME # Since I run v 4.9.2 (newer than 4.4)
+   conda activate "${CONDA_ENV_NAME}" # Since I run v 4.9.2 (newer than 4.4)
 
    #source activate $CONDA_ENV_NAME # For conda versions older than 4.4 the command 'source activate' 
 
