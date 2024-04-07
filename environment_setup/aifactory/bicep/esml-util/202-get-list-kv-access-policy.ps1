f26642fd24d8135ec0a151bf11d1de61e6f9159c
jostrm <jostrm@microsoft.com>
2024-04-07 23:18:29 +0200

    esml-util get, list kv

---
+++ environment_setup/aifactory/bicep/esml-util/202-get-list-kv-access-policy.ps1
@@ -0,0 +1,19 @@
+param (
+    # required parameters
+    [Parameter(Mandatory = $true, HelpMessage = "Specifies the secret for service principal, to login with")][string]$spSecret,
+    [Parameter(Mandatory=$true, HelpMessage="Specifies the app id for service principal, to login with")][string]$spID,
+    [Parameter(Mandatory = $true, HelpMessage = "Specifies the secret for service principal")][string]$tenantID,
+    [Parameter(Mandatory=$true, HelpMessage="Specifies the object id for service principal, to assign GET, LIST Access policy")][string]$target_spOID,
+    [Parameter(Mandatory = $false, HelpMessage = "ESML AIFactory keyvault name")][string]$keyvaultName,
+    [Parameter(Mandatory = $false, HelpMessage = "ESML AIFactory subscription id")][string]$subscription_id
+)
+$context = Get-AzSubscription -SubscriptionId $subscriptionID
+Set-AzContext $context
+
+# Login with the service principal
+$SecureStringPwd = $spSecret | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $spID, $SecureStringPwd
+Connect-AzAccount -ServicePrincipal -Credential $credential -Tenant $tenantID
+
+# Set the Key Vault access policy
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ObjectId $target_spOID -PermissionsToSecrets get,list -BypassObjectIdValidation
