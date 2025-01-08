df641e8ddf48dfaf36e31b085023551425f51b33
jostrm <jostrm@microsoft.com>
2025-01-08 15:12:01 +0100

    cpy templates

--- 01-init-template-files-once.sh
+++ 01-init-template-files-once.sh
@@ -111,37 +111,6 @@ mkdir -p "$aif_dir"
 # Copy template files
 start_dir="azure-enterprise-scale-ml"
 
-## TEMPLATES: DataOps, MLOps, GenAIOps
-if [ "$copy_notebooks" = true ]; then
-    mkdir -p "$aif_dir/mlops/01_template_v14/"
-    cp "$start_dir/mlops/01_template_v14/" "$aif_dir/mlops/01_template_v14/" -r # mlops
-
-    mkdir -p "$aif_dir/dataops/adf/"
-    cp "$start_dir/adf/v1_3/" "$aif_dir/dataops/adf/" -r
-    
-    mkdir -p "$aif_dir/notebook_aml_v1_templates/"
-    cp "$start_dir/notebook_templates/1_quickstart/" "$aif_dir/notebook_aml_v1_templates/" -r
-    
-    mkdir -p "$aif_dir/notebook_aml_v2_examples/"
-    cp "$start_dir/notebook_templates/notebook_aml_sdkv2_versus_sdkv1/" "$aif_dir/notebook_aml_v2_examples/" -r
-
-    mkdir -p "$aif_dir/notebook_aml_v2_examples/model_diabetes/"
-    cp "$start_dir/notebook_templates/model_diabetes/" "$aif_dir/notebook_aml_v2_examples/model_diabetes/" -r
-
-    mkdir -p "$aif_dir/notebook_databricks/"
-    cp "$start_dir/notebook_templates/notebook_databricks/" "$aif_dir/notebook_databricks/" -r
-fi
-
-if [ "$init_parameters" = true ]; then
-    ## PARAMETERS - BICEP/TERRAFORM/ADO/GHA
-    cp "$start_dir/environment_setup/aifactory/parameters/" "$aif_dir/parameters/" -r
-fi
-
-if [ "$init_esml_util" = true ]; then
-    ## Util (Bicep, Powershell, Azure CLI)
-    cp "$start_dir/environment_setup/aifactory/bicep/esml-util/" "$aif_dir/esml-util/" -r
-fi
-
 ## TEMPLATES: infra orchestration (pipelines) - ADO (Bicep)
 mkdir -p "$aif_dir/esml-infra/azure-devops/bicep/classic/"
 cp "$start_dir/environment_setup/aifactory/bicep/copy_to_local_settings/azure-devops/esml-ado-pipelines/" "$aif_dir/esml-infra/azure-devops/bicep/classic/" -r
