f11b012a53a174672b888ab51a82df6c9d7bdfed
jostrm <jostrm@microsoft.com>
2025-01-09 02:55:51 +0100

    bootstrap

--- 00-start.sh
+++ 00-start.sh
@@ -7,6 +7,8 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Copy template file and bootstrap files
 cp "$SCRIPT_DIR/01-aif-copy-aifactory-templates.sh" "$SCRIPT_DIR/../01-aif-copy-aifactory-templates.sh"
 cp "$SCRIPT_DIR/02a-GH-bootstrap-files.sh" "$SCRIPT_DIR/../02a-GH-bootstrap-files.sh"
 cp "$SCRIPT_DIR/02b-ADO-bootstrap-files.sh" "$SCRIPT_DIR/../02b-ADO-bootstrap-files.sh"
