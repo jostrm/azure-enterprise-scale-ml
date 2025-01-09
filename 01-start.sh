e6f807d090fb0bb7bc96aea2fec027ca00c0b8b6
jostrm <jostrm@microsoft.com>
2025-01-09 01:32:02 +0100

    copy files

--- 01-start.sh
+++ 01-start.sh
@@ -7,4 +7,4 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cp "$SCRIPT_DIR/01-init-template-files-once.sh" "$SCRIPT_DIR/../01-copy-iac-templates-ado-gha.sh"
+cp "$SCRIPT_DIR/01-copy-iac-templates-ado-gha.sh" "$SCRIPT_DIR/../01-copy-iac-templates-ado-gha.sh"
