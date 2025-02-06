39cbc611f0fd354a9a3b161c638a70b5d2d2a939
jostrm <jostrm@microsoft.com>
2025-02-06 23:32:41 +0100

    Update bootstrap

--- bootstrap/02a-GH-bootstrap-files.sh
+++ bootstrap/02a-GH-bootstrap-files.sh
@@ -11,28 +11,36 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "$SCRIPT_DIR/.github/workflows/"
 
+# Check if the directory exists
+if [ -d "$SCRIPT_DIR/aifactory/esml-infra/github-actions/bicep/" ]; then
+  # Delete all files in the directory
+  rm -rf "$SCRIPT_DIR/aifactory/esml-infra/github-actions/bicep/"
+fi
+# Create the directory if it does not exist
+mkdir -p "$SCRIPT_DIR/aifactory/esml-infra/github-actions/bicep/"
+
 # .ENV file & 03a-GH-create-or-update-github-variables.sh
 cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/.env.template" "$SCRIPT_DIR/.env"
 cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/03a-GH-create-or-update-github-variables.sh" "$SCRIPT_DIR/10-GH-create-or-update-github-variables.sh"
 
-# YAML - Common -> aifactory-templates + .gihub/workflows
-cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-common.yml" "$SCRIPT_DIR/aifactory-templates/esml-infra/github-actions/bicep/infra-common.yml"
+# YAML - Common -> aifactory + .gihub/workflows
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-common.yml" "$SCRIPT_DIR/aifactory/esml-infra/github-actions/bicep/infra-common.yml"
 cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-common.yml" "$SCRIPT_DIR/.github/workflows/infra-common.yml"
 
-# YAML - infra-project-esml.yml -> aifactory-templates + .gihub/workflows
-cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-project-esml.yml" "$SCRIPT_DIR/aifactory-templates/esml-infra/github-actions/bicep/infra-project-esml.yml"
+# YAML - infra-project-esml.yml -> aifactory + .gihub/workflows
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-project-esml.yml" "$SCRIPT_DIR/aifactory/esml-infra/github-actions/bicep/infra-project-esml.yml"
 cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-project-esml.yml" "$SCRIPT_DIR/.github/workflows/infra-project-esml.yml"
 
-# YAML - infra-project-genai.yml -> aifactory-templates + .gihub/workflows
-cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-project-genai.yml" "$SCRIPT_DIR/aifactory-templates/esml-infra/github-actions/bicep/infra-project-genai.yml"
+# YAML - infra-project-genai.yml -> aifactory + .gihub/workflows
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-project-genai.yml" "$SCRIPT_DIR/aifactory/esml-infra/github-actions/bicep/infra-project-genai.yml"
 cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-project-genai.yml" "$SCRIPT_DIR/.github/workflows/infra-project-genai.yml"
 
-# YAML - infra-add-project-member.yml -> aifactory-templates + .gihub/workflows
-cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-add-project-member.yml" "$SCRIPT_DIR/aifactory-templates/esml-infra/github-actions/bicep/infra-add-project-member.yml"
+# YAML - infra-add-project-member.yml -> aifactory + .gihub/workflows
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-add-project-member.yml" "$SCRIPT_DIR/aifactory/esml-infra/github-actions/bicep/infra-add-project-member.yml"
 cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-add-project-member.yml" "$SCRIPT_DIR/.github/workflows/infra-add-project-member.yml"
 
-# YAML - infra-add-core-member.yml -> aifactory-templates + .gihub/workflows
-cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-add-core-member.yml" "$SCRIPT_DIR/aifactory-templates/esml-infra/github-actions/bicep/infra-add-core-member.yml"
+# YAML - infra-add-core-member.yml -> aifactory + .gihub/workflows
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-add-core-member.yml" "$SCRIPT_DIR/aifactory/esml-infra/github-actions/bicep/infra-add-core-member.yml"
 cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/github-actions/infra-add-core-member.yml" "$SCRIPT_DIR/.github/workflows/infra-add-core-member.yml"
 
 echo -e "${GREEN}Success! ${NC}"
--- bootstrap/03b-ADO-YAML-bootstrap-files-no-var-overwrite.sh
+++ bootstrap/03b-ADO-YAML-bootstrap-files-no-var-overwrite.sh
@@ -8,9 +8,33 @@ NC='\033[0m' # No Color
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Check if the directory exists, if not, create it
+if [ ! -d "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/variables/" ]; then
+  mkdir -p "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/variables/"
+fi
+
+# Check if the directory exists
+if [ -d "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/esml-infra-common/" ]; then
+  # Delete all files in the directory
+  rm -rf "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/esml-infra-common/"
+fi
+
+# Create the directory if it does not exist
+mkdir -p "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/esml-infra-common/"
+
+# Check if the directory exists
+if [ -d "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/esml-infra-project/" ]; then
+  # Delete all files in the directory
+  rm -rf "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/esml-infra-project/"
+fi
+
+# Create the directory if it does not exist
+mkdir -p "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/esml-infra-project/"
+
 # variables.yaml -> variables-template.yaml
-cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/azure-devops/esml-yaml-pipelines/variables/variables.yaml" "$SCRIPT_DIR/aifactory-templates/esml-infra/azure-devops/yaml/variables/variables-template.yaml"
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/azure-devops/esml-yaml-pipelines/variables/variables.yaml" "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/variables/variables-template.yaml"
 
 # Copy the YAML files:esml-infra-common, 
-cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/azure-devops/esml-yaml-pipelines/esml-infra-common/" "$SCRIPT_DIR/aifactory-templates/esml-infra/azure-devops/yaml/esml-infra-common/" -r
-cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/azure-devops/esml-yaml-pipelines/esml-infra-project/" "$SCRIPT_DIR/aifactory-templates/esml-infra/azure-devops/yaml/esml-infra-project/" -r
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/azure-devops/esml-yaml-pipelines/esml-infra-common/" "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/esml-infra-common/" -r
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/azure-devops/esml-yaml-pipelines/esml-infra-project/" "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/esml-infra-project/" -r
+cp "$SCRIPT_DIR/azure-enterprise-scale-ml/environment_setup/aifactory/bicep/copy_to_local_settings/azure-devops/esml-yaml-pipelines/readme.md" "$SCRIPT_DIR/aifactory/esml-infra/azure-devops/bicep/yaml/readme.md"
